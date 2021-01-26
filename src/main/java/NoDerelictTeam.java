189f4e01de3dd1a8c7ba3e66ba07a337fedcf0ca
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-26 17:29:50 +0300

    Initial commit

---
+++ src/main/java/NoDerelictTeam.java
@@ -0,0 +1,54 @@
+import arc.util.Timer;
+import mindustry.Vars;
+import mindustry.game.Team;
+import mindustry.game.Teams;
+import mindustry.gen.Groups;
+import mindustry.gen.Player;
+import mindustry.mod.Plugin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NoDerelictTeam extends Plugin {
+
+    @Override
+    public void init() {
+        Timer.schedule(() -> {
+
+            HashMap<Team, Integer> amount = new HashMap<>();
+            for (Teams.TeamData team : Vars.state.teams.active) {
+                if (team.team == Team.derelict) continue;
+                amount.put(team.team, 0);
+            }
+
+            ArrayList<Player> players = new ArrayList<>();
+            Groups.player.each(player -> {
+                if (player.team() == Team.derelict) {
+                    players.add(player);
+                } else {
+                    int count = amount.get(player.team());
+                    amount.put(player.team(), count + 1);
+                }
+            });
+
+            if (players.size() > 0) {
+                List<Map.Entry<Team, Integer>> list = new ArrayList<>(amount.entrySet());
+                list.sort(Map.Entry.comparingByValue());
+
+                for (Player player : players) {
+                    Map.Entry<Team, Integer> lastEntry = list.get(0);
+                    for (Map.Entry<Team, Integer> entry : list) {
+                        if (lastEntry.getValue() < entry.getValue()) {
+                            break;
+                        }
+                        lastEntry = entry;
+                    }
+                    player.team(lastEntry.getKey());
+                }
+            }
+
+        }, 0, 1);
+    }
+}
