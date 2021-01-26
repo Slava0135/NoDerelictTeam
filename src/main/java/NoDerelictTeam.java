c435c2e434d6791f41b2f78e7eadd77338110e29
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-26 17:35:07 +0300

    omg

--- src/main/java/NoDerelictTeam.java
+++ src/main/java/NoDerelictTeam.java
@@ -6,10 +6,7 @@ import mindustry.gen.Groups;
 import mindustry.gen.Player;
 import mindustry.mod.Plugin;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class NoDerelictTeam extends Plugin {
 
@@ -34,21 +31,27 @@ public class NoDerelictTeam extends Plugin {
             });
 
             if (players.size() > 0) {
+
                 List<Map.Entry<Team, Integer>> list = new ArrayList<>(amount.entrySet());
                 list.sort(Map.Entry.comparingByValue());
 
                 for (Player player : players) {
+
                     Map.Entry<Team, Integer> lastEntry = list.get(0);
+                    int index = 0;
                     for (Map.Entry<Team, Integer> entry : list) {
                         if (lastEntry.getValue() < entry.getValue()) {
                             break;
                         }
                         lastEntry = entry;
+                        index++;
                     }
+
+                    Map.Entry<Team, Integer> newEntry = new AbstractMap.SimpleEntry<>(lastEntry.getKey(), lastEntry.getValue() + 1);
+                    list.set(index,  newEntry);
                     player.team(lastEntry.getKey());
                 }
             }
-
         }, 0, 1);
     }
 }
